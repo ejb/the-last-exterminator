a5188591d28de488bdb3193c8f6aa474c462d1da
ejb <mail@elliotbentley.com>
2015-01-10 16:42:17 +0000

    Use URL params to define level

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 
-var Game = function(){
+var Game = function(levels){
     var that = this;
+    this.levels = levels;
     document.onreadystatechange = function () {
         if (document.readyState == "complete") {
             that.run();
@@ -10,48 +11,44 @@ var Game = function(){
 
 Game.prototype.run = function(){
     var that = this;
-    this.startLevel(0, 'win');
+    if (!getURLParameter()) {
+        setURLParameter(1);
+    }
+    this.startLevel( getURLParameter()-1 );
 }
 
-Game.prototype.endLevel = function(){
-    console.log(this.currLevelId);
-    document.querySelector('#game_container'+this.currLevelId).innerHTML = '';
-    return this;
-}
 
-Game.prototype.startLevel = function(lno, endstate){
+Game.prototype.startLevel = function(lno){
     var that = this;
     var ldetails = this.levels[lno];
-    if (ldetails.started) {
-        return;
-    }
-        
-    if (endstate === 'win') {
-        var callback = function(endstate){
-            that.endLevel().startLevel(lno+1, endstate);
-        }
-    } else {
-        var callback = function(endstate){
-            that.endLevel().startLevel(lno, endstate);
+    
+    var callback = function(endstate){
+        var thisLevel = getURLParameter();
+        if (endstate === 'win') {
+            setURLParameter( thisLevel+1 );
+        } else {
+            setURLParameter( thisLevel );
         }
     }
 
-    this.levels[lno].started = true;
-    setTimeout(function(){
-        that.levels[lno].started = false;
-    }, 2000);
-
-    this.currLevelId = ((Math.random() * 1000000000)+'').replace('.','');
     var el = document.createElement('div');
-    el.setAttribute('id','game_container'+this.currLevelId);
+    el.setAttribute('id','game_container');
     document.body.appendChild(el);
-    this.runningLevel = new Level(callback, this.currLevelId, ldetails);
 
-    return this.runningLevel;
+    new Level(callback, ldetails);
 
 }
 
-Game.prototype.levels = [
-    {},
-    {}
-];
+
+function getURLParameter() {
+    var n = decodeURIComponent(
+        (RegExp('[?|&]level=' + '(.+?)(&|$)').exec(location.search)||[null,null])[1]
+    );
+    n = +(n.replace('/',''));
+    return n;
+}
+function setURLParameter(value){
+    var search = '?level=' + value;
+    window.location.href = window.location.href.split('?')[0] + search;
+}
+
---
+++ js/level.js
@@ -0,0 +1,417 @@
+var Level = function(callback, opts){
+    var that = this;
+    this.callback = callback;
+    this.opts = opts;
+    this.run();
+};
+
+Level.prototype.bounds = {
+    x: 800,
+    y: 600
+}
+
+Level.prototype.run = function(){
+    var that = this;
+    this.game = new PixelJS.Engine();
+    this.game.init({
+        container: ('game_container'),
+        width: this.bounds.x,
+        height: this.bounds.y
+    });
+        
+        
+    this.addBackground();
+    this.addPlayer();
+    this.stockBullets();
+    this.spawnBugs();
+    this.spawnBuildings();
+    this.spawnCollapsedBuildings();
+
+    this.playerLayer.registerCollidable(this.player);
+    this.enemies.forEach(function(en, i){
+        that.enemyLayer.registerCollidable(en);
+    });
+    this.bullets.forEach(function(bullet, i){
+        that.bulletLayer.registerCollidable(bullet);
+    });
+    this.buildings.forEach(function(building, i){
+        that.buildingLayer.registerCollidable(building);
+    });
+
+    /* TESTBED 
+    
+    var bullet = this.bulletLayer.createEntity();
+    bullet.pos = { x: 500, y: 150 };
+    bullet.size = { width: 12, height: 16 };
+    bullet.asset = new PixelJS.AnimatedSprite();
+    bullet.asset.prepare({
+        name: 'coin.png',
+        frames: 8,
+        rows: 1,
+        speed: 80,
+        defaultFrame: 0
+    });
+    /* END */
+
+
+
+
+
+
+    this.game.loadAndRun(function (elapsedTime, dt) {
+        
+        that.elapsedTime = elapsedTime;
+        that.dt = dt;
+        
+        that.player.$updateDirection();
+        
+        that.enemies.forEach(function(enemy){
+            enemyAi(enemy, that);
+        });
+        
+        that.bullets.forEach(function(bullet, i){    
+            bullet.$move();
+        });
+        
+        function enemyAi(en, that){
+            var enemy = en;
+            if (enemy.direction.y) {
+                enemy.moveUp();
+            } else {
+                enemy.moveDown();
+            }
+        
+            if (enemy.direction.x) {
+                enemy.moveLeft();
+            } else {
+                enemy.moveRight();
+            }
+        
+        
+        
+            var pos = enemy.pos;
+            if (pos.y < 50) {
+                enemy.direction.y = false;
+            } else if (pos.y > that.bounds.y - 50) {
+                enemy.direction.y = true;
+            }
+
+
+            if (pos.x < 50) {
+                enemy.direction.x = false;
+            } else if (pos.x > that.bounds.x - 50) {
+                enemy.direction.x = true;
+            }
+            
+        }
+
+    });
+    
+    
+}
+
+Level.prototype.addBackground = function(){
+    var game = this.game;
+    var backgroundLayer = game.createLayer('background');
+    var floor = backgroundLayer.createEntity();
+    backgroundLayer.static = true;
+    floor.pos = { x: 0, y: 0 };
+    floor.asset = new PixelJS.Tile();
+    floor.asset.prepare({
+        name: 'floor.png',
+        size: { 
+            width: 800, 
+            height: 600 
+        }
+    });
+    
+}
+
+Level.prototype.spawnBugs = function(){
+    this.enemyLayer = this.game.createLayer('enemies');
+    var numberOfBugs = this.opts.enemies;
+    for (var i = 0; i < numberOfBugs; i++) {
+        this.spawnBug();
+    }
+}
+
+Level.prototype.spawnBuildings = function(){
+    this.buildingLayer = this.game.createLayer('buildings');
+    var numberOfBuildings = 5;
+    for (var i = 0; i < numberOfBuildings; i++) {
+        this.spawnBuilding();
+    }
+}
+
+Level.prototype.spawnCollapsedBuildings = function(){
+    var numberOfCollapsedBuildings = 5;
+    for (var i = 0; i < numberOfCollapsedBuildings; i++) {
+        this.spawnCollapsedBuilding();
+    }
+}
+
+Level.prototype.playerHealth = 3;
+Level.prototype.addPlayer = function(){
+    var that = this;
+    var game = this.game;
+    var playerLayer = game.createLayer('players');
+    var player = new PixelJS.Player();
+    player.addToLayer(playerLayer);
+    player.pos = { x: 200, y: 300 };
+    player.size = { width: 32, height: 32 };
+    player.velocity = { x: 100, y: 100 };
+    player.asset = new PixelJS.AnimatedSprite();
+    player.asset.prepare({ 
+        name: 'player_sheet.png', 
+        frames: 4, 
+        rows: 4,
+        speed: 70,
+        defaultFrame: 1
+    });
+    
+    player.onCollide(this.playerCollision.bind(this));
+    
+    
+    game.on('keyDown', function (keyCode) {
+        if (keyCode === PixelJS.Keys.Space) {
+            console.log('fire!');
+            // console.log(that.spawnBullet);
+            that.fireBullet( that.player.pos );
+        }
+    });
+
+    player.$lastDirection = PixelJS.Directions.Down;
+    player.$updateDirection = function(){
+        if (player.direction !== 0) {
+            player.$lastDirection = player.direction;
+        }
+    }
+
+
+
+    this.playerLayer = playerLayer;
+    this.player = player;
+}
+
+Level.prototype.enemies = [];
+Level.prototype.spawnBug = function(){
+    var game = this.game;
+    var enemy = new PixelJS.Entity();
+    enemy.addToLayer(this.enemyLayer);
+    var x = Math.random() * this.bounds.x;
+    var y = Math.random() * this.bounds.y;
+    enemy.pos = { x: x, y: y };
+    enemy.size = { width: 32, height: 32 };
+    enemy.velocity = { x: 50, y: 50 };
+    enemy.asset = new PixelJS.AnimatedSprite();
+    enemy.asset.prepare({ 
+        name: 'bug_sheet.png',
+        frames: 2,
+        rows: 4,
+        speed: 100,
+        defaultFrame: 1
+    });
+    this.enemies.push(enemy);
+    enemy.direction = {
+        y: true,
+        x: true
+    }
+}
+
+Level.prototype.buildings = [];
+Level.prototype.spawnBuilding = function(){
+    var that = this;
+    var game = this.game;
+    var building = new PixelJS.Entity();
+    building.addToLayer(this.buildingLayer);
+    var x = Math.random() * this.bounds.x;
+    var y = Math.random() * this.bounds.y;
+    building.pos = { x: x, y: y };
+    building.size = { width: 128, height: 128 };
+    building.velocity = { x: 50, y: 50 };
+    building.asset = new PixelJS.Sprite();
+    building.asset.prepare({ 
+        name: 'building.png', 
+        frames: 2,
+        rows: 1,
+        speed: 100,
+        defaultFrame: 0
+    });
+    building.$collapsed = false;
+    building.$collapse = function(index){
+        if (this.$collapsed) {
+            return;
+        }
+        that.collapsedBuildings[index].moveTo(this.pos.x, this.pos.y);
+        this.dispose();
+    }
+    this.buildings.push(building);
+    building.direction = {
+        y: true,
+        x: true
+    }
+}
+
+Level.prototype.collapsedBuildings = [];
+Level.prototype.spawnCollapsedBuilding = function(){
+    var game = this.game;
+    var building = new PixelJS.Entity();
+    building.addToLayer(this.buildingLayer);
+    building.pos = { x: -1000, y: -1000 };
+    building.size = { width: 128, height: 128 };
+    building.asset = new PixelJS.Sprite();
+    building.asset.prepare({ 
+        name: 'building2.png', 
+        frames: 2,
+        rows: 1,
+        speed: 100,
+        defaultFrame: 0
+    });
+    this.collapsedBuildings.push(building);
+}
+
+Level.prototype.recentlyHit = false;
+Level.prototype.playerCollision = function(entity){
+    var that = this;
+    this.recentlyHit = checkIfStillTimeout(this.recentlyHit, this.dt);
+    var enemies = this.enemies;
+    // console.log(this.recentlyHit);
+    if (this.recentlyHit === false) {
+        for (var i = 0; i < enemies.length; i++) {
+            if (entity === enemies[i]) {
+                this.playerHealth--;
+                this.endLevel('fail');
+                console.log('Ouch! Health now at '+this.playerHealth);
+                this.recentlyHit = this.dt;
+                // jumpBack(that.player);
+            }
+        }
+    }
+        
+    function checkIfStillTimeout(rh, dt){
+        if (rh < dt - 0.01) {
+            return false;
+        }
+        return dt;
+    }
+    
+    function jumpBack(player){
+        var pos = player.pos;
+        if (player.direction == PixelJS.Directions.Left) {
+            console.log('Player is facing left');
+            pos.x += 50;
+        } else if (player.direction == PixelJS.Directions.Right) {
+            console.log('Player is facing right');
+            pos.x -= 50;
+        }
+        player.moveTo( pos.x, pos.y, 1000 );
+        
+    }
+
+}
+
+Level.prototype.bullets = [];
+Level.prototype.stockBullets = function(){
+    var that = this;
+    this.bulletLayer = this.game.createLayer('bullets');
+    var bullet;
+    for (var i = 0; i < 50; i++) {
+        
+        bullet = this.bulletLayer.createEntity();
+        bullet.pos = { x: -20, y: -20 };
+        bullet.size = { width: 12, height: 16 };
+        bullet.asset = new PixelJS.AnimatedSprite();
+        bullet.asset.prepare({
+            name: 'coin.png',
+            frames: 8,
+            rows: 1,
+            speed: 80,
+            defaultFrame: 0
+        });
+        
+        bullet.onCollide(this.bulletImpact.bind(this));
+    
+    
+        bullet.$move = function(){
+            var dir = this.$direction;
+            switch (dir) {
+            case PixelJS.Directions.Right:
+                this.pos.x += 10;
+                break;            
+            case PixelJS.Directions.Left:
+                this.pos.x -= 10;
+                break;            
+            case PixelJS.Directions.Up:
+                this.pos.y -= 10;
+                break;            
+            case PixelJS.Directions.Down:
+                this.pos.y += 10;
+                break;            
+            }
+            this.moveTo(this.pos.x, this.pos.y);
+            
+        }
+        bullet.$end = function(){
+            this.pos = { x: -20, y: -20 };
+            this.$fired = false;
+        }
+        bullet.$fired = false;
+        // bullet.update = bullet.$move;
+        this.bullets.push(bullet);
+        
+    }
+    
+}
+Level.prototype.fireBullet = function(newPos){
+    var that = this;
+    var bullets = this.bullets;
+    var fired = false;
+    bullets.forEach(function(bullet){
+        if (!fired && !bullet.$fired) {
+            bullet.pos = {
+                x: newPos.x,
+                y: newPos.y
+            };
+            bullet.$direction = that.player.$lastDirection;
+            bullet.$fired = true;
+            setTimeout(function(){
+                bullet.$end();
+            },3000);
+            fired = true;
+        }
+    })
+}
+
+Level.prototype.bulletImpact = function(entity){
+    var that = this;
+    var enemies = this.enemies;
+    var buildings = this.buildings;
+    // console.log(enemies);
+    for (var i = 0; i < enemies.length; i++) {
+        if (entity === enemies[i]) {
+            var enemy = enemies[i];
+            // console.log('Bug hit!');
+            enemy.dispose();
+            this.enemies.splice(i, 1);
+            console.log('xxxx', this.enemies, this.enemies.length);
+            if (this.enemies.length === 0) {
+                console.log(this.endLevel);
+                this.endLevel('win');
+            }
+        }
+    }
+    var buildingsHit = [];
+    for (var i = 0; i < buildings.length; i++) {
+        if ((entity === buildings[i])) {
+            var building = buildings[i];
+            building.$collapse(i);
+        }
+    }
+
+}
+
+Level.prototype.endLevel = function(state){
+    console.log(this);
+    this.callback(state);
+    console.log(this);
+}
